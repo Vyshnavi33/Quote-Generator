fda440e6f5ebc3d15bd2b2e33722af55b79b28a0
vyshnavi <vyshnaviraj33@gmail.com>
2024-03-16 11:59:58 +0530

    Added loading spinner functionality

--- script.js
+++ script.js
@@ -4,14 +4,28 @@
  const authorText = document.getElementById('author'); 
  const twitterBtn = document.getElementById('twitter'); 
  const newQuoteBtn = document.getElementById('new-quote'); 
+ const loader = document.getElementById('loader');
 
  
 
  let apiQuotes = [];
 
+ function showLoadingSpinner(){
+    loader.hidden=false;
+    quoteContainer.hidden=true;
+ }
+
+function removeLoadingSpinner(){
+    quoteContainer.hidden=false;
+    loader.hidden=true;
+}
+
 //Show new quote
 function newQuote(){
+    showLoadingSpinner();
+    //Pick a random quote from apiQuotes
     const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+    //Check if author field is blank and replace it with 'Unknown'
     if(!quote.author){
         authorText.textContent='Unknown';
     }else{
@@ -23,11 +37,14 @@ function newQuote(){
     }else{
         quoteText.classList.remove('long-quote');
     }
+    //Set Quote, Hide Loader
     quoteText.textContent=quote.text;
+    removeLoadingSpinner();
 }
 
 // Get Quotes From API
 async function getQuotes(){
+    showLoadingSpinner();
     const apiUrl = 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiUrl);
